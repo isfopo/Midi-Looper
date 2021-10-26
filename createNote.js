b263f8c4723e2dae225e0113e89c6e5e3f4ff274
Isaac Poole <isaacpoolemusic@gmail.com>
2021-10-26 18:35:18 -0400

    toggles between play and overdub

--- createNote.js
+++ createNote.js
@@ -19,12 +19,15 @@ max.addHandler("noteOn", (pitch, start_time, abs_start_time, velocity) => {
 max.addHandler("noteOff", (pitch, abs_end_time) => {
   if (notes.length > 0) {
     const note = notes.filter(note => note.pitch === pitch)[0]; // should loop thru notes
-    max.outlet({ 
-      pitch: note.pitch, 
-      start_time: note.start_time, 
-      duration: abs_end_time - note.abs_start_time,
-      velocity: note.velocity
-    });
-    notes = notes.filter(note => note.pitch !== pitch);
+    max.post(note);
+    if (note) {
+      max.outlet({ 
+        pitch: note.pitch, 
+        start_time: note.start_time, 
+        duration: abs_end_time - note.abs_start_time,
+        velocity: note.velocity
+      });
+      notes = notes.filter(note => note.pitch !== pitch);
+    }
   }
 });
